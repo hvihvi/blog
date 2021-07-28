db587d102906f885b24170e4085a7c4168d6e3f3
hvi <hvi@lesfurets.com>
2021-07-28 11:06:32 +0200

    Replace name

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,7 +5,7 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
+    title: 'Vinet Hugo',
     site_url: 'https://yoursite.com',
     feed_url: 'https://yoursite.com/feed.xml'
   })
