2e4406f4fb6fe59ff0b7f6e4e2aab32ba63af8b6
Hugo Vinet <hugo.vinet@koober.com>
2022-03-29 14:23:49 +0200

    test redir

--- pages/api/redirect.ts
+++ pages/api/redirect.ts
@@ -6,10 +6,5 @@ export default function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  response.status(302).json({
-    body: 'hello world',
-    location: 'koober://koob/25',
-    query: request.query,
-    cookies: request.cookies
-  })
+  response.redirect(302, 'koober://koob/25')
 }
