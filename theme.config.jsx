54f05c231c04c5299b40c2daa924f71d172e1947
Hugo VINET <hvi@hugos-macbook-pro.home>
2023-08-18 00:17:48 +0200

    upgrade nextra

---
+++ theme.config.jsx
@@ -0,0 +1,20 @@
+export default {
+  head: ({ title, meta }) => (
+    <>
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
+      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.author && <meta name="author" content={meta.author} />}
+    </>
+  ),
+  readMore: 'Read More →',
+  postFooter: null,
+  darkMode: false,
+  navs: [
+    {
+      url: 'https://github.com/shuding/nextra',
+      name: 'Nextra'
+    }
+  ]
+}
